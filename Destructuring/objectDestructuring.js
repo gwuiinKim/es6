3881e2bd2bdebd3814d92b194b047968fe89d296
gwuiinkim <gwuiinkim@gmail.com>
2019-11-02 23:03:48 +0900

    destructuring in function argument .

--- Destructuring/objectDestructuring.js
+++ Destructuring/objectDestructuring.js
@@ -24,23 +24,28 @@ const {
 
 // default value when destructuring object.
 
-let chosenColor = "green"
+let chosenColor = "green";
 
+// desturcture and assign to let variable is possible
+({
+  color: { chosen_color: chosenColor }
+} = settings);
 
-  // desturcture and assign to let variable is possible
-  ({
-    color: { chosen_color: chosenColor }
-  }) = settings
-
+console.log(chosenColor);
 
 // const saveSettings = (followAlert, unFollowAlert, marketingAlert, themeColor) =>
 // argument without key,value is not good.
 
-const saveSettings = settings => {
+const saveSettings = ({ follow, alert, mkt, color = "blue" }) => {
   console.log(settings);
 };
 
 saveSettings({
+  // true,
+  // true,
+  //true ---> it is bad .
+  // just listing argument is very bad convention.
+  // should give description.
   follow: true,
   alert: true,
   mkt: true,
